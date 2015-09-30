d240c7e0984f77b8542450ba44921d1667d8da91
bestlook <brunoteixeiralc@gmail.com>
2015-09-30 01:55:23 -0300

    Commit 30/09 - add progress dialog

--- app/src/main/java/br/com/compracombinada/activity/WebViewCadastrar.java
+++ app/src/main/java/br/com/compracombinada/activity/WebViewCadastrar.java
@@ -1,8 +1,12 @@
 package br.com.compracombinada.activity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import br.com.compracombinada.R;
 
@@ -10,16 +14,41 @@ import br.com.compracombinada.R;
 public class WebViewCadastrar extends Activity {
 
     private WebView webView;
+    private ProgressBar progressBar;
+    private static final String URL = "http://ec2-52-21-177-220.compute-1.amazonaws.com/combobox/form_usuario/?tp_cadastro={SITE}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview_cadastrar);
-
+        progressBar = (ProgressBar)findViewById(R.id.progress);
         webView = (WebView) findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://ec2-52-21-177-220.compute-1.amazonaws.com/combobox/form_usuario/?tp_cadastro={SITE}");
+        setWebViewClient(webView);
+        webView.loadUrl(URL);
 
     }
 
+    private void setWebViewClient(WebView webview) {
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+                super.onPageStarted(webview, url, favicon);
+                // Liga o progress
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView webview, String url) {
+                // Desliga o progress
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+
+    }
 }
